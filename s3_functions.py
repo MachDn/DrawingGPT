434731743f2cae38cae7022b968f01312f1c29ac
SumitKumarSahoo <x21154589@student.ncirl.ie>
2022-04-29 14:12:47 +0100

    added boto3 session

--- s3_functions.py
+++ s3_functions.py
@@ -1,15 +1,25 @@
 import boto3
+import os
+import re
+from botocore.exceptions import ClientError
+
 
 # Function to Upload Image to S3
 def upload_file(file_name, bucket):
     object_name = file_name
-    s3_client = boto3.client('s3')
+    session = boto3.Session(region_name='eu-west-3',
+		                aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
+		                aws_secret_access_key=os.environ['AWS_SECRET_KEY'])
+    s3_client = session.client('s3')
     response = s3_client.upload_file(file_name, bucket, object_name)
     return response
 
 # Function to generate public URL from S3 to show images on the pages 
 def show_image(bucket):
-    s3_client = boto3.client('s3')
+    session = boto3.Session(region_name='eu-west-3',
+		                aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
+		                aws_secret_access_key=os.environ['AWS_SECRET_KEY'])
+    s3_client = session.client('s3')
     public_urls = []
     try:
         for item in s3_client.list_objects(Bucket=bucket)['Contents']:
