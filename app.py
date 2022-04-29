a81da00f2a88749c7f4248cf8bc9a0858f99315f
SumitKumarSahoo <x21154589@student.ncirl.ie>
2022-04-29 11:29:27 +0100

    made UI changes, removed credentials

--- app.py
+++ app.py
@@ -15,12 +15,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tgif.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# s3 = boto3.client('s3',
-#                     aws_access_key_id="ASIATUYJP7SUIUKCDXNS",
-#                     aws_secret_access_key= "iGtiVUNmtepq6zwt9sZo04HAdpOB/XOrunS4YEOL",
-#                     aws_session_token="IQoJb3JpZ2luX2VjECIaCXVzLWVhc3QtMSJHMEUCIBVUNpme6/qQUWDKW5PL0RsJVqmxINVGVa7NWKoc2f9yAiEAogrZked06FtTMHyb3jByV5qxH28KtcWzYsnpFpoXcVQqlAQI2///////////ARACGgwyNTA3Mzg2Mzc5OTIiDM5PktwH+uWBXAQkuyroAzr6u7qZs0nOyfuLWu5Mo4C2e3J0hYUOWVZIw95n7PR6zJHVuw2m319LeWMK1A+Fy4fa0//RFeWzAzy/B3vJHhp10X+BWNjDn8uklSA5dRRvNE6RfZsJLi1+HMQXzyVYanIRiAKxJIt0fFjGfkmlTgicawb34QEt8Vq0Z/T7EgP+SJtkbQqLIdU/9/QbThJofBlzFJ4VNGqEUSPlgo0w/wAZMtcwNp6teenDc8bSYh6PadOPN3cBHDZVNdEchRngWVeF5b0eov1W6nUp/+4h9VdIUN0fSc+wP5DeFN7lG5MYC/nVQoJgmrGOj2F7xyHKLPjkgUUcZ0nHQzT0VbyKIkk9KSBt92y4kN8K64ula5v5Dtix4G5ghMY7Ou6e9RIkuS5JSLavuJD5XuBcuYdtgNwaxLSfTx7y9bzB9ZLJ0iiOCgrduIhAgr5LdJaTekcG6INwb8/s+LpzkBil6eK0XH+emGSlDLQ02WqkfrZJcu2e3JJC0uBfy6jYX2mMBicAkPBOeh8fDoj8JEHVrfcvucJi9ePEeXeIWlROAdlPSRk1wkuNZBqQu4nSVMERVjOVccDLOsWIYWIvxn0GiiKC3jAxuCLiAoWeuGraGNtJ1PNl8hU/E4ec5LKEzoGojNkDOM/5ca9vENirMKinq5MGOqYBR/Otve5Kq3K4J8QBYQe+HUv8cfhkk9fNegnw0D+uzKHi6pxUQuzjrXhcXa/VJic+CrR4I+IEylenVmcFnkiJAor9uAPTNJaMvk40P5oTCGrJr/jLtGoPY9fPFYEDx36hUWUS8AATQxXbtqONFUTNpX5PgdodRfbpOiYEUYUbUC2FNoPD8m+H/mLaR3nd4z+/REMgeV1Jz+ZSGTSgsmyFarnyFx0OHQ=="
-#                      )
-
 # S3 bucket and folder holding image details
 UPLOAD_FOLDER = "uploads"
 BUCKET = "21154589-cpp-project"
